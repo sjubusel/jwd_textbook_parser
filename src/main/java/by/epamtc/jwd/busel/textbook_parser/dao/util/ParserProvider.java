b6c0559073498ea84b0ed99b7d1180833def9cd5
Siarhei Busiel <sju.busel@gmail.com>
2020-07-28 12:14:24 +0300

    Correct TODO-s

--- src/main/java/by/epamtc/jwd/busel/textbook_parser/dao/util/ParserProvider.java
+++ src/main/java/by/epamtc/jwd/busel/textbook_parser/dao/util/ParserProvider.java
@@ -27,6 +27,6 @@ public class ParserProvider {
 
     public void parseAndUpdate(String line, Text text) {
         line = line.replaceAll("\\s+", " ");
-        //TODO depending on whether codeblock of text line FORWARD to certain chains of parsers
+        //TODO depending on whether code block or text line FORWARD to certain chains of parsers
     }
 }
